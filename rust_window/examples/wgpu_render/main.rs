c1814bbe710f8c64dc2e63519c7b314b7f671119
royeeliu <royeeliu@qq.com>
2024-08-03 22:39:25 +0800

    [rust_window]Render backgound color use WGPU

---
+++ rust_window/examples/wgpu_render/main.rs
@@ -0,0 +1,150 @@
+use std::sync::Arc;
+
+use winit::{
+    application::ApplicationHandler,
+    dpi::PhysicalSize,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, EventLoop},
+    window::{Window, WindowId},
+};
+
+#[allow(dead_code)]
+struct WgpuContext<'window> {
+    surface: wgpu::Surface<'window>,
+    surface_config: wgpu::SurfaceConfiguration,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+}
+
+impl<'window> WgpuContext<'window> {
+    pub fn new(window: Arc<Window>) -> Self {
+        pollster::block_on(WgpuContext::new_async(window))
+    }
+
+    pub async fn new_async(window: Arc<Window>) -> WgpuContext<'window> {
+        let instance = wgpu::Instance::default();
+        println!("WGPU instance created.");
+        let surface = instance.create_surface(window.clone()).unwrap();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .expect("Request adapter failed");
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::default(),
+                    memory_hints: wgpu::MemoryHints::default(),
+                },
+                None,
+            )
+            .await
+            .expect("Request device failed");
+        let size = window.inner_size();
+        let width = size.width.max(1);
+        let height = size.height.max(1);
+        let surface_config = surface.get_default_config(&adapter, width, height).unwrap();
+        surface.configure(&device, &surface_config);
+        WgpuContext {
+            surface,
+            surface_config,
+            adapter,
+            device,
+            queue,
+        }
+    }
+
+    pub fn draw(&mut self) {
+        let texture = self
+            .surface
+            .get_current_texture()
+            .expect("Get current texture failed");
+        let texture_view = texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &texture_view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
+        self.queue.submit(Some(encoder.finish()));
+        texture.present();
+    }
+
+    pub fn resize(&mut self, size: PhysicalSize<u32>) {
+        self.surface_config.width = size.width.max(1);
+        self.surface_config.height = size.height.max(1);
+        self.surface.configure(&self.device, &self.surface_config);
+    }
+}
+
+#[derive(Default)]
+struct App<'window> {
+    window: Option<Arc<Window>>,
+    context: Option<WgpuContext<'window>>,
+}
+
+impl<'window> ApplicationHandler for App<'window> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        println!("Resumed");
+        if self.window.is_none() {
+            let attr = Window::default_attributes().with_title("WGPU Render Example");
+            let window = event_loop.create_window(attr).unwrap();
+            let window = Arc::new(window);
+            let context = WgpuContext::new(window.clone());
+            self.window = Some(window);
+            self.context = Some(context);
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::Resized(size) => {
+                if let (Some(window), Some(context)) = (self.window.as_ref(), self.context.as_mut())
+                {
+                    context.resize(size);
+                    window.request_redraw();
+                }
+            }
+            WindowEvent::RedrawRequested => {
+                if let Some(context) = self.context.as_mut() {
+                    context.draw();
+                }
+            }
+            WindowEvent::CloseRequested => {
+                println!("Close requested");
+                event_loop.exit();
+            }
+            _ => (),
+        }
+    }
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    let mut app = App::default();
+    event_loop.run_app(&mut app).expect("Run app failed");
+}
