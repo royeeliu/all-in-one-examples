74bbfa39ac1783c5dc608a34be08aeea24cbcae6
royeeliu <royeeliu@qq.com>
2024-12-07 12:00:25 +0800

    [rust_ffmpeg]Support link static library

---
+++ rust_ffmpeg/use-ffmpeg-next/simple_play/build.rs
@@ -0,0 +1,16 @@
+#[cfg(windows)]
+fn link_system_libs() {
+    println!("cargo:rustc-link-lib=Gdi32");
+    println!("cargo:rustc-link-lib=OleAut32");
+    println!("cargo:rustc-link-lib=Shlwapi");
+    println!("cargo:rustc-link-lib=Mfuuid");
+    println!("cargo:rustc-link-lib=Strmiids");
+    println!("cargo:rustc-link-lib=Vfw32");
+}
+
+#[cfg(not(windows))]
+fn link_system_libs() {}
+
+fn main() {
+    link_system_libs();
+}
